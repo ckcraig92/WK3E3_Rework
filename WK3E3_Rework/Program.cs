0108ed62847f1dffca859045de21959d7cde1f20
ckcraig@amarillocollege.com <ckcraig@amarillocollege.com>
2025-04-24 11:20:50 -0500

    Add project files.

---
+++ WK3E3_Rework/Program.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WK3E3_Rework
+{
+    internal class Program
+    {
+        //creating methods
+        public static void ReverseString();
+        public static void CountVowels();
+        public static void IsPalindrome();
+
+        static void Main(string[] args)
+        {//declaration
+            string userInput;
+
+
+            //input
+            Console.WriteLine("Input a string");
+            userInput = Console.ReadLine();
+
+            {
+                //menu
+                Console.WriteLine("Operations Menu:");
+                Console.WriteLine(" 1. Reverse String");
+                Console.WriteLine(" 2. Count Vowels");
+                Console.WriteLine(" 3. Is Palindrome");
+                Console.WriteLine("Select an option from the menu above");
+
+            }
+        }
+        //process
+        switch()
+            {
+            case 1:
+            ReverseString();
+            break;
+            case 2:
+            CountVowels();
+            break;
+            case 3:
+            IsPlaindrome();
+            break;
+            }
+
+
+            //process reverse string
+            ReverseString();
+        //process count vowels
+        CountVowels();
+
+               
+            {
+        int vowelCount = 0;
+        foreach (char c in input.ToLower())
+        {
+        if ("aeiou".Contains(c))
+        {
+        vowelCount++;
+       }
+}
+
+                //Output
+                Console.WriteLine("The number of vowels in the string is: " + vowelCount);
+
+//process palindrome
+IsPalindrome();
+
+    
+} internal class Program
+    {
+        //creating methods
+        public static void ReverseString();
+        public static void CountVowels();
+        public static void IsPalindrome();
+
+        static void Main(string[] args)
+        {//declaration
+            string userInput;
+
+
+            //input
+            Console.WriteLine("Input a string");
+            userInput = Console.ReadLine();
+
+            {
+                //menu
+                Console.WriteLine("Operations Menu:");
+                Console.WriteLine(" 1. Reverse String");
+                Console.WriteLine(" 2. Count Vowels");
+                Console.WriteLine(" 3. Is Palindrome");
+                Console.WriteLine("Select an option from the menu above");
+
+            }
+        }
+        //process
+        switch()
+            {
+            case 1:
+            ReverseString();
+            break;
+            case 2:
+            CountVowels();
+            break;
+            case 3:
+            IsPlaindrome();
+            break;
+            }
+
+
+            //process reverse string
+            ReverseString();
+        //process count vowels
+        CountVowels();
+
+               
+            {
+        int vowelCount = 0;
+        foreach (char c in input.ToLower())
+        {
+        if ("aeiou".Contains(c))
+        {
+        vowelCount++;
+       }
+}
+
+                //Output
+                Console.WriteLine("The number of vowels in the string is: " + vowelCount);
+
+//process palindrome
+IsPalindrome();
+
+    
+} internal class Program
+    {
+        //creating methods
+        public static void ReverseString();
+        public static void CountVowels();
+        public static void IsPalindrome();
+
+        static void Main(string[] args)
+        {//declaration
+            string userInput;
+
+
+            //input
+            Console.WriteLine("Input a string");
+            userInput = Console.ReadLine();
+
+            {
+                //menu
+                Console.WriteLine("Operations Menu:");
+                Console.WriteLine(" 1. Reverse String");
+                Console.WriteLine(" 2. Count Vowels");
+                Console.WriteLine(" 3. Is Palindrome");
+                Console.WriteLine("Select an option from the menu above");
+
+            }
+        }
+        //process
+        switch()
+            {
+            case 1:
+            ReverseString();
+            break;
+            case 2:
+            CountVowels();
+            break;
+            case 3:
+            IsPlaindrome();
+            break;
+            }
+
+
+            //process reverse string
+            ReverseString();
+        //process count vowels
+        CountVowels();
+
+               
+            {
+        int vowelCount = 0;
+        foreach (char c in input.ToLower())
+        {
+        if ("aeiou".Contains(c))
+        {
+        vowelCount++;
+       }
+}
+
+                //Output
+                Console.WriteLine("The number of vowels in the string is: " + vowelCount);
+
+//process palindrome
+IsPalindrome();
+
+    
+}
+
---
+++ WK3E3_Rework/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WK3E3_Rework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WK3E3_Rework")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ffc6b846-91b9-4380-8b54-b4ca8cce7bf7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
