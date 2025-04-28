a22f15291f61c6398beb87781217cb54ae00cca4
ckcraig@amarillocollege.com <ckcraig@amarillocollege.com>
2025-04-27 20:12:37 -0500

    update

--- WK3E3_Rework/Program.cs
+++ WK3E3_Rework/Program.cs
@@ -3,82 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using Microsoft.Win32;
 
 namespace WK3E3_Rework
 {
     internal class Program
     {
         //creating methods
-        public static void ReverseString();
-        public static void CountVowels();
-        public static void IsPalindrome();
+        public static void ReverseString(string userInput)
+        {
+            char[] charArray = userInput.ToCharArray(); //convert the string to an array
+            Array.Reverse(charArray); //reversing the array
+            Console.WriteLine("The reversed string is: " + new string(charArray)); //print for user to view
 
-        static void Main(string[] args)
-        {//declaration
-            string userInput;
-
-
-            //input
-            Console.WriteLine("Input a string");
-            userInput = Console.ReadLine();
-
-            {
-                //menu
-                Console.WriteLine("Operations Menu:");
-                Console.WriteLine(" 1. Reverse String");
-                Console.WriteLine(" 2. Count Vowels");
-                Console.WriteLine(" 3. Is Palindrome");
-                Console.WriteLine("Select an option from the menu above");
-
-            }
         }
-        //process
-        switch()
-            {
-            case 1:
-            ReverseString();
-            break;
-            case 2:
-            CountVowels();
-            break;
-            case 3:
-            IsPlaindrome();
-            break;
-            }
 
-
-            //process reverse string
-            ReverseString();
-        //process count vowels
-        CountVowels();
-
-               
-            {
-        int vowelCount = 0;
-        foreach (char c in input.ToLower())
-        {
-        if ("aeiou".Contains(c))
-        {
-        vowelCount++;
-       }
-}
-
-                //Output
-                Console.WriteLine("The number of vowels in the string is: " + vowelCount);
-
-//process palindrome
-IsPalindrome();
-
-    
-} internal class Program
-    {
         //creating methods
-        public static void ReverseString();
-        public static void CountVowels();
-        public static void IsPalindrome();
+        public static void CountVowels(string userInput)
+        {
+            int vowelCount = 0; //starting the count at 0
+            foreach (char c in userInput.ToLower()) //looping through each character
+            {
+                if ("aeiou".Contains(c)) //listing the vowels
+                {
+                    vowelCount++;
+                }
+            }
+            Console.WriteLine("The vowel count in the string is: " + vowelCount); //print for user to view
+        }
+
+        public static void IsPalindrome(string userInput)
+        {
+            char[] charArray = userInput.ToCharArray(); //create array
+            Array.Reverse(charArray);// reversing
+            string reveresed = new string(charArray); //new string
+
+            if (userInput == reveresed) //testing
+            {
+                Console.WriteLine("The string is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not a palindrome.");
+            }
+        }
 
         static void Main(string[] args)
-        {//declaration
+        {
+            //declaration
             string userInput;
 
 
@@ -94,109 +67,27 @@ IsPalindrome();
                 Console.WriteLine(" 3. Is Palindrome");
                 Console.WriteLine("Select an option from the menu above");
 
+
+            }
+            //process
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    ReverseString(userInput);
+                    break;
+                case 2:
+                    CountVowels(userInput);
+                    break;
+                case 3:
+                    IsPalindrome(userInput);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
             }
         }
-        //process
-        switch()
-            {
-            case 1:
-            ReverseString();
-            break;
-            case 2:
-            CountVowels();
-            break;
-            case 3:
-            IsPlaindrome();
-            break;
-            }
+    }
 
-
-            //process reverse string
-            ReverseString();
-        //process count vowels
-        CountVowels();
-
-               
-            {
-        int vowelCount = 0;
-        foreach (char c in input.ToLower())
-        {
-        if ("aeiou".Contains(c))
-        {
-        vowelCount++;
-       }
-}
-
-                //Output
-                Console.WriteLine("The number of vowels in the string is: " + vowelCount);
-
-//process palindrome
-IsPalindrome();
-
-    
-} internal class Program
-    {
-        //creating methods
-        public static void ReverseString();
-        public static void CountVowels();
-        public static void IsPalindrome();
-
-        static void Main(string[] args)
-        {//declaration
-            string userInput;
-
-
-            //input
-            Console.WriteLine("Input a string");
-            userInput = Console.ReadLine();
-
-            {
-                //menu
-                Console.WriteLine("Operations Menu:");
-                Console.WriteLine(" 1. Reverse String");
-                Console.WriteLine(" 2. Count Vowels");
-                Console.WriteLine(" 3. Is Palindrome");
-                Console.WriteLine("Select an option from the menu above");
-
-            }
-        }
-        //process
-        switch()
-            {
-            case 1:
-            ReverseString();
-            break;
-            case 2:
-            CountVowels();
-            break;
-            case 3:
-            IsPlaindrome();
-            break;
-            }
-
-
-            //process reverse string
-            ReverseString();
-        //process count vowels
-        CountVowels();
-
-               
-            {
-        int vowelCount = 0;
-        foreach (char c in input.ToLower())
-        {
-        if ("aeiou".Contains(c))
-        {
-        vowelCount++;
-       }
-}
-
-                //Output
-                Console.WriteLine("The number of vowels in the string is: " + vowelCount);
-
-//process palindrome
-IsPalindrome();
-
-    
-}
-
+    }
